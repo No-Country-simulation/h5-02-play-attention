502fb7354c50f58da226d272d5ae6b03da519649
Tolomatico <ballesty.t@gmail.com>
2025-04-22 14:13:59 -0300

    Added Auth and Users to swagger

--- backend/src/auth/dto/register-auth.dto.ts
+++ backend/src/auth/dto/register-auth.dto.ts
@@ -1,16 +1,21 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 import { UserRole, Services } from '../../users/schema/user.schema';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
+  @ApiProperty({ example: 'juan@mail.com', description: 'Correo electrónico del usuario' })
   @IsEmail({}, { message: 'El email no es válido' })
   email: string;
 
+  @ApiProperty({ example: '123456', description: 'Contraseña del usuario' })
   @MinLength(6, { message: 'El Password debe contener 6 caracteres' })
   password: string;
 
+  @ApiProperty({ example: 'Profesional', description: 'Tipo de servicio del usuario' })
   @IsNotEmpty({ message: 'Debes elegir el tipo de servicio' })
   service: Services;
 
+  @ApiProperty({example:"Profesional",description:"Rol del usuario"})
   @IsNotEmpty({ message: 'Debes elegir el rol del usuario' })
   role: UserRole;
 }
--- backend/src/leads/leads.module.ts
+++ backend/src/leads/leads.module.ts
@@ -3,6 +3,8 @@ import { LeadsController } from './leads.controller';
 import { LeadsService } from './leads.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Leads, LeadsSchema } from './schema/leads.model';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -11,7 +13,15 @@ import { Leads, LeadsSchema } from './schema/leads.model';
         name: Leads.name,
         schema: LeadsSchema
       }
-    ])
+    ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>("jwt.secret"),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [LeadsController],
   providers: [LeadsService]
