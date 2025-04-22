cb5c44e11fb0bc296655e8f54c4470d98b978011
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2025-04-22 18:05:28 -0500

    [chore] back: whitelist global validation pipes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -25,10 +25,14 @@ async function bootstrap() {
       'playAttentionToken',
     )
     .build();
-  
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
