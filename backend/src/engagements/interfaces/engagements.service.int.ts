b194fd5b77cdea737b3ab0cba51a3297b4996d7e
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2025-04-23 23:59:18 -0500

    [feat] back: add generate engagement from landing form

--- backend/src/engagements/interfaces/engagements.service.int.ts
+++ backend/src/engagements/interfaces/engagements.service.int.ts
@@ -1,11 +1,18 @@
 import { PaginationResponseDto } from 'src/common/dtos/pagination.dto';
-import { GenerateEngagementDto } from '../dto/generate-engagement.dto';
+import {
+  GenerateEngagementDto,
+  GenerateEngagementFromLandingDto,
+} from '../dto/generate-engagement.dto';
 import { UpdateEngagementDto } from '../dto/update-engagement.dto';
 import { Engagements } from '../schema/engagements.schema';
 
 export interface IEngagementService {
   generateEngagement(
     generateEngagementItemDto: GenerateEngagementDto,
+  ): Promise<Engagements>;
+
+  generateEngagementFormLanding(
+    dto: GenerateEngagementFromLandingDto,
   ): Promise<Engagements>;
 
   getEngagementsByLeadId(
