87511af51b4becb97e5dc43abaef93628fa4cea7
BenjaMoreno <webenja0@gmail.com>
2025-05-01 21:35:12 -0400

    direccionar a whatsapp al pinchar boton

--- frontend/landing/src/features/contact/staticContact.jsx
+++ frontend/landing/src/features/contact/staticContact.jsx
@@ -23,6 +23,9 @@ const iconClasses =
   "bg-[#cfcfe0] rounded-full w-6 h-6 p-1 text-[#5C3983] mx-2 stroke-[2px] md:w-7 md:h-7";
 
 export const StaticContact = () => {
+  const handleClick = () => {
+    window.open("https://wa.me/56940551892", "_blank", "noopener,noreferrer");
+  };
   return (
     <div className="w-full  md:w-[29%] flex flex-col gap-8 p-4 md:p-0">
       {/* Contenedor de información de contacto */}
@@ -43,13 +46,16 @@ export const StaticContact = () => {
       </div>
 
       <div className="w-full flex flex-col sm:flex-row gap-4 mt-4 md:mt-0">
-        <Button className="w-full sm:w-[46.24%] h-12">
+        <Button
+          onClick={handleClick}
+          className="w-full sm:w-[46.24%] h-12 cursor-pointer"
+        >
           <MessageCircle className="size-5 md:size-6 mr-2" />
           Whatsapp
         </Button>
         <Button
           variant="outline"
-          className="w-full sm:w-[46.24%] h-12 border-2 border-[#330764] text-[#330764] hover:bg-[#330764]/10"
+          className="w-full sm:w-[46.24%] h-12 border-2 border-[#330764] text-[#330764] hover:bg-[#330764]/10 cursor-pointer"
         >
           <CalendarDays className="size-5 md:size-6 mr-2" />
           Agendar reunión
