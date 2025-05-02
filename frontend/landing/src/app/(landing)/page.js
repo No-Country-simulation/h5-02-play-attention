35df81b94851795b7e320a8e0b7cb88be92a2145
jfpanchi <jefpanchi@gmail.com>
2025-05-02 18:44:13 -0500

    feat: ✨ Add section about us
    Add section about us

--- frontend/landing/src/app/(landing)/page.js
+++ frontend/landing/src/app/(landing)/page.js
@@ -3,7 +3,7 @@ import { ContactSection } from "@/features/contact/contact";
 import { HeroSection } from "@/features/hero/hero";
 import { TestimonialsSection } from "@/features/testimonials/testimonials";
 import { DemoVideosSection } from "@/features/demo-videos/demo-videos";
-/* import { AboutUsSection } from "@/shared/components/about-us/about-us"; */
+import { AboutUsSection } from "@/shared/components/about-us/about-us";
 /* import { PricingPlansSection } from "@/features/pricing-plans/pricing-plans"; */
 
 export default function Home() {
@@ -25,6 +25,11 @@ export default function Home() {
         {/* <PricingPlansSection /> */}
       </section>
 
+      {/* About Us */}
+      <section id="about-us">
+        <AboutUsSection />
+      </section>
+
       {/* Testimonials */}
       <section id="testimonials">
         <TestimonialsSection />
@@ -34,11 +39,6 @@ export default function Home() {
       <section id="contact">
         <ContactSection />
       </section>
-
-      {/* About Us */}
-      {/*  <section id="about-us">
-        <AboutUsSection />
-      </section> */}
     </main>
   );
 }
