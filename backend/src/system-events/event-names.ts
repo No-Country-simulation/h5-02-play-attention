6350ec937d1d5e0585e3691e216323c51e370fa3
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2025-05-06 15:36:48 -0500

    [update] back: add event names

--- backend/src/system-events/event-names.ts
+++ backend/src/system-events/event-names.ts
@@ -1,8 +1,21 @@
 export const LEAD_EVENTS = {
-  LEAD_CREATED: 'lead.created'
+  LEAD_CREATED: 'lead.created',
 } as const;
 
-export const USER_EVENTS={
+export const USER_EVENTS = {
   USER_CREATED: 'user.created',
-  FORGOT_PASSWORD: 'user.forgotPassword'
+  USER_REGISTERED: 'user.created',
+  FORGOT_PASSWORD: 'user.forgotPassword',
+  USER_NEW_SEND_CREDENTIALS: 'user.new-send-credentials',
 } as const;
+
+export const NOTIFICATIONS = {
+  CREATE_SINGLE: 'notifications.create-single',
+  CREATE_MANY: 'notifications.create-many',
+  EMAIL_PLAIN_SEND_MANY: 'notifications.email.plain-send-many',
+  WS_SEND_MANY: 'notifications.ws.send-many',
+} as const;
+
+export const SUP_TICKETS = {
+  USER_CREATED: 'sup-tickets.user-created',
+};
