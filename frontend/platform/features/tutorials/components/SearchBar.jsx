9ee74c109a36d7074492ed2e794388deade69ef9
jfpanchi <jefpanchi@gmail.com>
2025-05-07 09:15:28 -0500

    refactor: 💄 Refactor hiddle category filter
    Refactor hiddle category filter in section tutorials

--- frontend/platform/features/tutorials/components/SearchBar.jsx
+++ frontend/platform/features/tutorials/components/SearchBar.jsx
@@ -138,7 +138,7 @@ export function SearchBar({
           </div>
 
           {/* Selector de Categoría */}
-          <div className='relative'>
+         {/*  <div className='relative'>
             <button className='px-4 py-2 border border-gray-300 bg-white rounded-md flex items-center gap-2'>
               <Filter />
               Categoría
@@ -154,7 +154,7 @@ export function SearchBar({
                 </option>
               ))}
             </select>
-          </div>
+          </div> */}
 
           {/* Selector de Fecha */}
           <div className='relative'>
