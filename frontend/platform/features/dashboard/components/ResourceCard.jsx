878bb7220b0cb9f0f5241af2ed1530a34224e13b
BenjaMoreno <webenja0@gmail.com>
2025-05-07 18:19:59 -0400

    agregamos iconos

--- frontend/platform/features/dashboard/components/ResourceCard.jsx
+++ frontend/platform/features/dashboard/components/ResourceCard.jsx
@@ -1,26 +1,37 @@
 "use client";
-
-import Link from "next/link";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardHeader,
   CardTitle,
 } from "@/shared/ui/card";
-import { Lightbulb } from "lucide-react";
+import Image from "next/image";
 
 export default function ResourceCard({ category }) {
-  const { title, description, icon: Icon, path, color } = category;
+  const { title, description, icon, path, color } = category;
 
   return (
-    <Card className="hover:shadow-md transition-shadow !bg-[#E9E9F1] h-[12vh] w-[21.1vw] mr-7 flex border border-[#838394] rounded-sm">
-      <div className={`w-20 flex items-center justify-center`}>
-        <Lightbulb className="h-[50%] w-auto stroke-[#15032A] " />
+    <Card
+      className={`hover:shadow-md transition-shadow !bg-[#E9E9F1] h-[12vh] w-[21.1vw] mr-7 flex border border-[#838394] rounded-sm`}
+    >
+      <div className={`w-20 flex items-center justify-center `}>
+        {icon.type === "lucide" ? (
+          <icon.component className={`w-8 h-8 text-${color}-600`} />
+        ) : (
+          <Image
+            src={icon.component}
+            alt={title}
+            width={32}
+            height={32}
+            className="object-contain"
+          />
+        )}
       </div>
       <CardContent className="flex flex-col justify-center h-full w-full !p-0">
-        <CardTitle className="text-xl text-[#15032A] mb-1">{title}</CardTitle>
-        <CardDescription className="text-xs text-[#656573]-500">
+        <CardTitle className={`text-xl text-${color}-800 mb-1`}>
+          {title}
+        </CardTitle>
+        <CardDescription className={`text-xs text-${color}-700`}>
           {description}
         </CardDescription>
       </CardContent>
--- frontend/platform/features/dashboard/components/ResourceExploreCard.jsx
+++ frontend/platform/features/dashboard/components/ResourceExploreCard.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import {
   Card,
   CardContent,
@@ -10,25 +11,31 @@ import {
 } from "@/shared/ui/card";
 
 export default function ResourceExploreCard({ category }) {
-  const { title, description, icon: Icon, path, color } = category;
+  const { title, description, icon: Icon, path } = category;
 
   return (
-    <Card className="hover:shadow-md transition-shadow w-[13.8vw] h-[26.9vh] mr-4 p-4 border border-[#C6C6DA] rounded-sm flex flex-col ">
-      <div className="bg-[#E9E9F1] p-1">
-        <Icon className="h-5 w-5 stroke-[#15032A]" />
+    <Card className="hover:shadow-md transition-shadow w-[13.8vw] h-[26.9vh] mr-4 p-4 border border-[#C6C6DA] rounded-sm flex flex-col !bg-[#F8F8FA]">
+      <div className="bg-[#DEDEEA] p-1 w-full h-8">
+        <Image
+          src={Icon}
+          alt={title}
+          width={20}
+          height={20}
+          className="text-[#15032A]"
+        />
       </div>
       <CardHeader className="!p-0 mt-2">
         <CardTitle className="!text-[1rem]">{title}</CardTitle>
       </CardHeader>
 
-      <CardContent className={"!p-0  mt-2"}>
-        <CardDescription className="text-xs text-[#A7A7BE] ">
+      <CardContent className={"!p-0 mt-2"}>
+        <CardDescription className="text-xs text-[#A7A7BE]">
           {description}
         </CardDescription>
       </CardContent>
       <Link
         href={path}
-        className="inline-block  text-[#15032A] hover:underline text-xs mt-auto"
+        className="inline-block text-[#15032A] hover:underline text-xs mt-auto"
       >
         <b>Explorar →</b>
       </Link>
